6d3bebb8aefb59900573ba425e58999d97bc5a25
eocampo52 <eocampo52@gmail.com>
2024-08-20 18:54:41 -0500

    added PATCH support to app

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const { v4: uuid } = require("uuid");
 
 // Parse data
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const comments = [
+  {
+    id: uuid(),
+    username: "Todd",
+    comment: "lol that was so funny",
+  },
+  {
+    id: uuid(),
+    username: "Jerry",
+    comment: "lol that was so funny",
+  },
+  {
+    id: uuid(),
+    username: "Eddie",
+    comment: "lol that was so funny",
+  },
+  {
+    id: uuid(),
+    username: "Eduar",
+    comment: "lol that was so funny",
+  },
+];
 
 // GET request
 
@@ -12,11 +39,41 @@ app.get("/tacos", (req, res) => {
   res.send("Get /tacos response");
 });
 
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
+// PATCH request
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find(c => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect('/comments')
+});
+
 // POST request
 
 app.post("/tacos", (req, res) => {
-  const {meat, qty} = req.body;
+  const { meat, qty } = req.body;
   res.send(`Ok, here are your ${qty} ${meat} tacos`);
+});
+
+app.post("/comments", (req, res) => {
+  const { comment, username } = req.body;
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
