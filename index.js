d4efba5c0421f3a7ffb267b9475d2526f6025847
eocampo52 <eocampo52@gmail.com>
2024-08-20 16:03:49 -0500

    msc commit

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/tacos', (req, res) => {
-  res.send('Get /tacos response')
-})
+// Parse data
 
-app.post('/tacos', (req, res) => {
-  res.send('POST /tacos response')
-})
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+// GET request
+
+app.get("/tacos", (req, res) => {
+  res.send("Get /tacos response");
+});
+
+// POST request
+
+app.post("/tacos", (req, res) => {
+  const {meat, qty} = req.body;
+  res.send(`Ok, here are your ${qty} ${meat} tacos`);
+});
 
 app.listen(3000, () => {
-  console.log('On port 3000')
-})
+  console.log("On port 3000");
+});
+
+// RESTful operations
+
+// GET /comments - list all comments
+// POST /comments - create a new comment
+
+// GET /comment:id - Get one comment (using ID)
+// Patch /comments/:id - update one comment
+// Delete /comments/:id - remove/delete one comment
