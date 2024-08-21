505e9f7c1852ef2d97b0e448936e4ea64104bef0
eocampo52 <eocampo52@gmail.com>
2024-08-20 19:38:14 -0500

    added delete CRUD operation

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuid } = require("uuid");
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 
 // Parse data
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const comments = [
+let comments = [
   {
     id: uuid(),
     username: "Todd",
@@ -57,7 +57,7 @@ app.get("/comments/:id", (req, res) => {
 
 app.get("/comments/:id/edit", (req, res) => {
   const { id } = req.params;
-  const comment = comments.find( c => c.id === id);
+  const comment = comments.find((c) => c.id === id);
   res.render("comments/edit", { comment });
 });
 
@@ -86,6 +86,13 @@ app.post("/comments", (req, res) => {
 
 app.listen(3000, () => {
   console.log("On port 3000");
+});
+
+// DELETE request
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter(c => c.id !== id);
+  res.redirect('/comments')
 });
 
 // RESTful operations
